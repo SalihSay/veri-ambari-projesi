b2a2d4fcaea1a6a14a35a7bb925bd5cdf50a2ead
Salih Say <129283902+SalihSay@users.noreply.github.com>
2025-08-04 17:07:00 +0300

    Create init_databese.sql

---
+++ scripts/init_databese.sql
@@ -0,0 +1,13 @@
+/* Projemde Medallion Mimarisini kullanmayı tercih ettim ve bronze, silver, gold olmak üzere 3 katman ayırdım. */
+
+use master;
+
+CREATE DATABASE DWH;
+
+USE DWH;
+
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
